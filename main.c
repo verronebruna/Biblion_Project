a3317fabbe75ceb030b74207fb8add23a61768be
brunaverrone <bruna.verrone@sumup.com>
2023-11-15 13:44:01 -0300

    update main code

--- main.c
+++ main.c
@@ -1,50 +1,26 @@
-/*
-Menu Principal e Submenus:
-    1 - Consulta de livros
-        Consultar livro por Título
-        Consultar livro por Autor
-        Consultar livro por Código ISBN
-        Retorna ao Menu principal
-
-    2 - Cadastro de livros
-        1- Aparece uma mensagem de aviso dos dados que serão necessários para a realização do cadastro do livro, os dados são os mesmos apresentados na opção 1.
-        2- Ao final do cadastro o sistema mostrará uma mensagem perguntando se o usuário quer cadastrar um novo livro ou retornar ao menu principal.
-
-    3 - Empréstimo de livros
-        1- aparecerá no sistema uma mensagem pedindo que o usuário digite o email, digitar o código ISBN do livro a ser emprestado
-            1- Se o livro estiver disponível registrar o empréstimo 
-               i. e perguntar se o usuário deseja pegar outro livro
-               ii. ou retornar ao menu principal.
-            2 - Caso o livro não esteja disponível, o sistema informa que o livro não está disponível e volta para o menu principal.
-
-    4 Relatórios de livros
-        1 - Relatório de livros disponíveis
-        2 - Relatório de livros emprestados
-        3 - Relatório de livros atrasados
-        4 - Voltar ao menu principal.
-
-    5 - Sair = programa se encerra
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
 
-// Definição da estrutura para os livros
-typedef struct listaLivros
+typedef struct lista_livros
 {
     char titulo[100];
     char autor[100];
     char ISBN[100];
-    int codigo;
-} Livro;
+    int disponivel;
+    int emprestado;
+} livro;
 
-// Vetor de livros com capacidade para 150 livros
-struct listaLivros livros[150];
+livro livros[150];
 
-// Função para o Menu Principal
+void limparBufferEntrada()
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
+}
+
 void MenuPrincipal()
 {
     printf("|-----------------------------------------------|\n");
@@ -61,181 +37,230 @@ void MenuPrincipal()
     printf("|           5 - Sair                            |\n");
     printf("|-----------------------------------------------|\n");
 }
-
 int main()
 {
-    setlocale(LC_ALL, "Portuguese"); // acentos funcionando
+    setlocale(LC_ALL, "Portuguese"); 
 
     int opcaoMenuPrincipal;
+    int opcaoCadastrar;
+    int opcaoRelatorios;
+    int opcaoConsultar;
+    int opcaoEmprestimo;
+    int encontrado = 0;
+    char isbnfornecido[150];
+    int numLivrosCadastrados = 0;
 
     do
     {
         MenuPrincipal();
         scanf("%d", &opcaoMenuPrincipal);
-    } while (opcaoMenuPrincipal < 1 || opcaoMenuPrincipal > 5);
-    // Cadastro de livros
-    int numLivrosCadastrados = 0;
-    int opcaoCadastrar;
-
-    // Relatórios de livros
-    int opcaoRelatorios;
-
-    if (opcaoMenuPrincipal == 1) // Consulta de livros
-    {
-        int opcaoConsultar;
-        printf("|-----------------------------------------------|\n");
-        printf("|       Você escolheu: 1 - Consultar livros.    |\n");
-        printf("|-----------------------------------------------|\n");
-        printf("|        Digite o número da opção desejada:     |\n");
-        printf("|-----------------------------------------------|\n");
-        printf("|         1 - Consultar livro por Título        |\n");
-        printf("|         2 - Consultar livro por Autor         |\n");
-        printf("|         3 - Consultar livro por Código ISBN   |\n");
-        printf("|         4 - Voltar ao menu principal          |\n");
-        printf("|-----------------------------------------------|\n");
-        scanf("%d", &opcaoConsultar);
-
-        for (int i = 0; i < opcaoConsultar; i++)
+        switch (opcaoMenuPrincipal)
         {
-            scanf("%s", livros[numLivrosCadastrados].titulo);
+        case 1:
             printf("|-----------------------------------------------|\n");
-            printf("|   O livro está disponível para empréstimo.    |\n");
-            printf("|         Deseja consultar outro livro?         |\n");
+            printf("|       Você escolheu: 1 - Consultar livros.    |\n");
             printf("|-----------------------------------------------|\n");
-            printf("|     1 - Sim, quero consultar outro livro.     |\n");
-            printf("|     2 - Não. Voltar ao menu principal         |\n");
+            printf("|        Digite o número da opção desejada:     |\n");
+            printf("|-----------------------------------------------|\n");
+            printf("|         1 - Consultar livro por Título        |\n");
+            printf("|         2 - Consultar livro por Autor         |\n");
+            printf("|         3 - Consultar livro por Código ISBN   |\n");
+            printf("|         4 - Voltar ao menu principal          |\n");
             printf("|-----------------------------------------------|\n");
             scanf("%d", &opcaoConsultar);
-            while (opcaoConsultar == 1)
-            {
-                // return opcaoConsultar;
-            }
-            while (opcaoConsultar == 2)
-            {
-                MenuPrincipal();
-            }
-        }
-        for (int i = 1; i < opcaoConsultar; i++)
-        {
-            printf("Digite o nome do autor do livro que deseja consultar: \n");
-            scanf("%s", livros[numLivrosCadastrados].autor);
-            // logica para ser implementada
-            //  if(livros nao existe?)
-            //  {
-            //      printf("Livro não encontrado. \n");
-            //  }
-            //  else if(livro existe && livro está disponível)
-            //  {
-            //      printf("O livro está disponível para empréstimo. \n");
-            //  }
-            //  else if(livro existe && livro não está disponível)
-            //  {
-            //      printf("O livro não está disponível para empréstimo. \n");
-            //  }
-            printf("O livro está disponível para empréstimo. \n");
-            printf("Deseja consultar outro livro? \n");
-            printf("1 - Sim \n");
-            printf("2 - Não. Voltar ao menu principal \n");
-            scanf("%d", &opcaoConsultar);
-            while (opcaoConsultar == 1)
-            {
-                // return opcaoConsultar;
-            }
-            while (opcaoConsultar == 2)
-            {
-                MenuPrincipal();
-            }
-        }
-        for (int i = 2; i < opcaoConsultar; i++)
-        {
+            int encontrado = 0;
 
-            printf("O livro está disponível para empréstimo. \n");
-            printf("Deseja consultar outro livro? \n");
-            printf("1 - Sim \n");
-            printf("2 - Não. Voltar ao menu principal \n");
-            scanf("%d", &opcaoConsultar);
-            while (opcaoConsultar == 1)
+            switch (opcaoConsultar)
             {
-                // return opcaoConsultar;
-            }
-            while (opcaoConsultar == 2)
-            {
-                MenuPrincipal();
-            }
-        }
-        for (int i = 3; i < opcaoConsultar; i++)
-        {
-            printf("Digite o código ISBN do livro que deseja consultar: \n");
-            scanf("%s", livros[numLivrosCadastrados].ISBN);
-            printf("O livro está disponível para empréstimo. \n");
-            printf("Deseja consultar outro livro? \n");
-            printf("1 - Sim \n");
-            printf("2 - Não. Voltar ao menu principal \n");
-            scanf("%d", &opcaoConsultar);
-            while (opcaoConsultar == 1)
-            {
-                // return opcaoConsultar;
-            }
-            while (opcaoConsultar == 2)
-            {
-                MenuPrincipal();
-            }
-        }
-        for (int i = 4; i < opcaoConsultar; i++)
-        {
-            MenuPrincipal();
-        }
-    }
-    else if (opcaoMenuPrincipal == 2) // Cadastro de livros
-    {
-        int opcaoCadastrar;
+            case 1:
+                printf("Digite o título do livro que deseja consultar: \n");
+                limparBufferEntrada();
+                char tituloConsulta[100];
+                fgets(tituloConsulta, sizeof(tituloConsulta), stdin);
+                tituloConsulta[strcspn(tituloConsulta, "\n")] = 0; 
 
-        printf("Você escolheu: 2 - Cadastrar livros. \n");
-        printf("Para cadastro de livros precisaremos de algumas informações, como titulo, autor e ISBN. \n");
-        printf("Digite o nome do livro que deseja cadastrar: \n");
-        scanf("%s", livros[numLivrosCadastrados].titulo);
-        printf("Digite o nome do autor do livro que deseja cadastrar: \n");
-        scanf("%s", livros[numLivrosCadastrados].autor);
-        printf("Digite o ISBN do livro que deseja cadastrar: \n");
-        scanf("%s", livros[numLivrosCadastrados].ISBN);
-        numLivrosCadastrados++;
-        printf("Livro cadastrado com sucesso! \n");
-        printf("Deseja cadastrar outro livro? \n");
-        printf("1 - Sim \n");
-        printf("2 - Não. Voltar ao menu principal \n");
-        scanf("%d", &opcaoCadastrar);
-        while (opcaoCadastrar == 1)
-        {
-            return opcaoCadastrar;
+                for (int i = 0; i < numLivrosCadastrados; i++)
+                {
+                    if (strcmp(livros[i].titulo, tituloConsulta) == 0)
+                    {
+                        printf("Livro encontrado - Título: %s, Autor: %s, ISBN: %s\n", livros[i].titulo, livros[i].autor, livros[i].ISBN);
+                        encontrado = 1;
+                    }
+                }
+
+                if (!encontrado)
+                {
+                    printf("Livro não encontrado.\n");
+                }
+                break;
+
+            case 2:
+                printf("Digite o nome do autor do livro que deseja consultar: \n");
+                limparBufferEntrada();
+                char autorConsulta[100];
+                fgets(autorConsulta, sizeof(autorConsulta), stdin);
+                autorConsulta[strcspn(autorConsulta, "\n")] = 0; 
+
+                int encontradoAutor = 0;
+                for (int i = 0; i < numLivrosCadastrados; i++)
+                {
+                    if (strcmp(livros[i].autor, autorConsulta) == 0)
+                    {
+                        printf("Livro encontrado - Título: %s, Autor: %s, ISBN: %s\n", livros[i].titulo, livros[i].autor, livros[i].ISBN);
+                        encontradoAutor = 1;
+                    }
+                }
+
+                if (!encontradoAutor)
+                {
+                    printf("Livro não encontrado.\n");
+                }
+                break;
+
+            case 3:
+                printf("Digite o código ISBN do livro que deseja consultar: \n");
+                limparBufferEntrada();
+                char isbnConsulta[100];
+                fgets(isbnConsulta, sizeof(isbnConsulta), stdin);
+                isbnConsulta[strcspn(isbnConsulta, "\n")] = 0; 
+
+                int encontradoISBN = 0;
+                for (int i = 0; i < numLivrosCadastrados; i++)
+                {
+                    if (strcmp(livros[i].ISBN, isbnConsulta) == 0)
+                    {
+                        printf("Livro encontrado - Título: %s, Autor: %s e ISBN: %s\n", livros[i].titulo, livros[i].autor, livros[i].ISBN);
+                        encontradoISBN = 1;
+                    }
+                }
+
+                if (!encontradoISBN)
+                {
+                    printf("Livro não encontrado.\n");
+                }
+                break;
+            }
+            break;
+
+        case 2:
+            printf("|-----------------------------------------------|\n");
+            printf("|     Você escolheu: 2 - Cadastrar livros.      |\n");
+            printf("|-----------------------------------------------|\n");
+            do
+            {
+                printf("|-----------------------------------------------|\n");
+                printf("|Para cadastro de livros precisaremos de algumas|\n");
+                printf("|   informações, como: titulo, autor e ISBN.    |\n");
+                printf("|-----------------------------------------------|\n");
+
+                printf("Digite o nome do livro que deseja cadastrar: \n");
+                limparBufferEntrada(); // Limpar o buffer de entrada
+                fgets(livros[numLivrosCadastrados].titulo, sizeof(livros[numLivrosCadastrados].titulo), stdin);
+                livros[numLivrosCadastrados].titulo[strcspn(livros[numLivrosCadastrados].titulo, "\n")] = 0; // Remover a quebra de linha
+
+                printf("Digite o nome do autor do livro que deseja cadastrar: \n");
+                fflush(stdin);
+                fgets(livros[numLivrosCadastrados].autor, sizeof(livros[numLivrosCadastrados].autor), stdin);
+                livros[numLivrosCadastrados].autor[strcspn(livros[numLivrosCadastrados].autor, "\n")] = 0;
+
+                printf("Digite o ISBN do livro que deseja cadastrar: \n");
+                fflush(stdin);
+                fgets(livros[numLivrosCadastrados].ISBN, sizeof(livros[numLivrosCadastrados].ISBN), stdin);
+                livros[numLivrosCadastrados].ISBN[strcspn(livros[numLivrosCadastrados].ISBN, "\n")] = 0;
+
+                // Inicializando o campo disponível
+                livros[numLivrosCadastrados].disponivel = 1; // não deveria começar com 0?
+                livros[numLivrosCadastrados].emprestado = 0; // Perguntar para o Bruno se é necessário inicializar o campo emprestado.
+
+                numLivrosCadastrados++;
+
+                if (numLivrosCadastrados >= 150)
+                {
+                    printf("Limite maximo de cadastros atingido. Nao e possivel cadastrar mais livros.\n");
+                }
+
+                printf("Livro cadastrado com sucesso! \n");
+                printf("Deseja cadastrar outro livro? \n");
+                printf("1 - Sim \n");
+                printf("2 - Não. Voltar ao menu principal \n");
+                scanf("%d", &opcaoMenuPrincipal);
+
+                while (opcaoMenuPrincipal != 1 && opcaoMenuPrincipal != 2)
+                {
+                    printf("Opção inválida. Digite 1 para cadastrar outro livro ou 2 para voltar ao menu principal.\n");
+                    scanf("%d", &opcaoMenuPrincipal);
+                }
+
+            } while (opcaoMenuPrincipal == 1);
+
+            break;
+
+        case 3: // Empréstimo de livros
+
+            opcaoEmprestimo = 1;
+            while (opcaoEmprestimo == 1)
+            {
+                char email[100];
+
+                printf("Digite o código ISBN do livro que deseja emprestar: ");
+                scanf(" %s", &isbnfornecido);
+                strcpy(livros[9].ISBN, "teste");
+                encontrado = 0;
+
+                for (int i = 0; i < 150; i++)
+                { // esse 'for' vai checar o ISBN fornecido com os ISBNs registrados.
+                    if (strcmp(livros[i].ISBN, isbnfornecido) == 0)
+                    {
+                        encontrado = 1;
+
+                        if (livros[i].emprestado == 0)
+                        { // as variaveis começam com o valor 0, então vai entrar nesse if se o livro não tiver sido emprestado antes
+                            printf("O livro está disponível para empréstimo!\nPor favor digite seu email para completar o empréstimo: ");
+                            scanf(" %s", email);
+                            livros[i].emprestado = 1; // o livro foi marcado como emprestado
+                            printf("O livro foi emprestado com sucesso.\n");
+                        }
+                        else
+                        {
+                            printf("O livro não está disponível para empréstimo.\n");
+                        }
+                        break; // isso quebra o for.
+                    }
+                }
+
+                if (encontrado == 0)
+                {
+                    printf("Livro não encontrado.\n");
+                }
+
+                printf("Deseja emprestar outro livro?\n");
+                printf("1 - Sim\n");
+                printf("2 - Não. Voltar ao menu principal\n");
+                scanf(" %d", &opcaoEmprestimo);
+                while (opcaoEmprestimo != 1 && opcaoEmprestimo != 2)
+                {
+                    printf("Valor inválido\n");
+                    printf("Deseja emprestar outro livro?\n");
+                    printf("1 - Sim\n");
+                    printf("2 - Não. Voltar ao menu principal\n");
+                    scanf(" %d", &opcaoEmprestimo);
+                }
+            }
+            break;
+        case 4: // Relatórios de livros
+            printf("Você escolheu: 4 - Relatórios de livros \n");
+            printf("Digite o número da opção desejada: \n");
+            printf("1 - Relatório de livros disponíveis \n");
+            printf("2 - Relatório de livros emprestados \n");
+            printf("3 - Relatório de livros atrasados \n");
+            scanf("%d", &opcaoRelatorios);
+            break;
+
+        case 5:
+            printf("Você escolheu: 5 - Sair. \n");
+            opcaoMenuPrincipal = 1;
         }
-        while (opcaoCadastrar == 2)
-        {
-            return MenuPrincipal;
-        }
-    }
-    else if (opcaoMenuPrincipal == 3) // Empréstimo de livros
-    {
-        // fluxo de empréstimo de livros para ser implementado
-        printf("Você escolheu: 3 - Empréstimo de livros. \n");
-    }
-    else if (opcaoMenuPrincipal == 4) // Relatórios de livros
-    {
-        // fluxo de relatórios de livros para ser implementado
-        printf("Você escolheu: 4 - Relatórios de livros \n");
-        printf("Digite o número da opção desejada: \n");
-        printf("1 - Relatório de livros disponíveis \n");
-        printf("2 - Relatório de livros emprestados \n");
-        printf("3 - Relatório de livros atrasados \n");
-        scanf("%d", &opcaoRelatorios);
-    }
-    else if (opcaoMenuPrincipal == 5) // Sair do programa
-    {
-        printf("Você escolheu: 5 - Sair. \n");
-    }
-    else
-    {
-        printf("Opção inválida. \n");
-    }
-    
+    } while (opcaoMenuPrincipal != 5);
+
     return 0;
 }
